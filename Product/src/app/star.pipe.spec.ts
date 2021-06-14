25c940add72bd0ded4235336b2027eb9567bdb0d
raksha303910 <raksha303910@gmail.com>
2021-06-15 00:49:45 +0530

    updated

--- Product/src/app/star.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StarPipe } from './star.pipe';
-
-describe('StarPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StarPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
